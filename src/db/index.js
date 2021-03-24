f2c6f892f1f048d049a7073c19e8c2bb44fa62bc
Sydney Sisco <hello@sydsis.co>
2021-03-24 14:48:21 -0700

    database connection change

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,10 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: `${process.env.DATABASE_URL}?ssl=true` || ""
+  connectionString: process.env.DATABASE_URL || "",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 client
