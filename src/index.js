bfc7441febac60bb02c2d2f11f038b622eb1a1dd
Sydney Sisco <hello@sydsis.co>
2022-10-15 17:00:30 -0700

    send turns to both players

--- src/index.js
+++ src/index.js
@@ -11,25 +11,28 @@ class Games {
 
   createGame(client) {
     const gameId = Math.floor(Math.random() * 900 + 100);
-    this.games.push({
+
+    this.games[gameId] = {
       id: gameId,
       host: client
-    });
+    };
 
     return gameId;
   }
 
   joinGame(client, gameId) {
-    const game = this.findGame(gameId);
+    const game = this.games[gameId];
+    console.log("game", game);
     if (game) {
       game.guest = client;
+
       return true;
     }
     return false;
   }
 
   findGame(id) {
-    return this.games.find(game => game.id === id);
+    return this.games[id];
   }
 }
 
@@ -57,30 +60,76 @@ const clients = new Clients();
 wss.on("connection", socket => {
   socket.onmessage = event => {
     console.log(`Message Received: ${event.data}`);
+    // socket.send(`Message Received: ${event.data}`);
     clients.saveClient(event.data.username, socket);
 
-    if (event.data.type === "CREATE_GAME") {
-      const gameID = games.createGame(socket);
-      socket.send(
-        JSON.stringify({
-          type: "CREATE_GAME",
-          gameID
-        })
-      );
-    }
+    try {
+      const message = JSON.parse(event.data);
+      console.log('message: ', message);
 
-    if (event.data.type === "JOIN_GAME") {
-      const success = games.joinGame(socket, event.data.gameID);
-      socket.send(
-        JSON.stringify({
-          type: "JOIN_GAME",
-          success
-        })
-      );
+      if (message.type === "CREATE_GAME") {
+        console.log("Creating game",);
+        const gameId = games.createGame(socket);
+        socket.send(
+          JSON.stringify({
+            type: "CREATE_GAME",
+            gameId
+          })
+        );
+      }
+
+      if (message.type === "JOIN_GAME") {
+        const success = games.joinGame(socket, message.gameId);
+        socket.send(
+          JSON.stringify({
+            type: "JOIN_GAME",
+            success
+          })
+        );
+        if (success) {
+          const game = games.findGame(message.gameId);
+          game.host.send(
+            JSON.stringify({
+              type: "GAME_READY",
+              gameId: message.gameId
+            })
+          );
+          game.guest.send(
+            JSON.stringify({
+              type: "GAME_READY",
+              gameId: message.gameId
+            })
+          );
+        }
+      }
+
+      if (message.type === "SEND_MOVE") {
+        const game = games.findGame(message.gameId);
+        const players = [game.host, game.guest];
+        console.log('sending move to players: ', players);
+        players.forEach(player => {
+          player.send(
+            JSON.stringify({
+              type: "MOVE",
+              move: message.move,
+              gameId: message.gameId
+            })
+          );
+        });
+
+      }
+
+    } catch (e) {
+      console.log(e); 
     }
+    
 
     if (event.data === "ping") {
       socket.send(JSON.stringify("pong"));
+    }
+
+    if (event.data === "{\"type\":\"ping\"}") {
+      socket.send(JSON.stringify("pong object"));
     }
   };
 });
@@ -97,6 +146,36 @@ function updateAppointment(id, interview) {
       );
     }
   });
+}
+
+function startGame(gameId, game) {
+  if (game.host.readyState === WebSocket.OPEN && game.guest.readyState === WebSocket.OPEN) {
+    const players = [game.host, game.guest];
+    players.forEach(player => {
+      player.send(
+        JSON.stringify({
+          type: "START_GAME",
+          gameId,
+          game
+        })
+      );
+    });
+  }
+}
+
+function updateGame(gameId, game) {
+  if (game.host.readyState === WebSocket.OPEN && game.guest.readyState === WebSocket.OPEN) {
+    const players = [game.host, game.guest];
+    players.forEach(player => {
+      player.send(
+        JSON.stringify({
+          type: "UPDATE_GAME",
+          gameId,
+          game
+        })
+      );
+    });
+  }
 }
 
 server.listen(PORT, () => {
